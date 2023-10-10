6d4d3d4114fd123977f52bd876f613cb7c8c6509
root <root@debian-couchdb-docker-compose-test>
2023-10-10 10:11:35 +0000

    Add setup scripts

---
+++ post-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f .env ]; then
+  echo "Exiting: Could not find .env file!"
+  exit 1
+fi
+
+ls -la
+echo " "
+
+set -a
+source .env
+set +a
+
+curl -X PUT http://"$COUCHDB_USER":"$COUCHDB_PASS"@localhost:5984/_users
+curl -X PUT http://"$COUCHDB_USER":"$COUCHDB_PASS"@localhost:5984/_replicator
